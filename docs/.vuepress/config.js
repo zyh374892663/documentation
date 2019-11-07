8cbac20b1278fc1a03abd3ad7f9b196a3cc9cbc0
leo_zhang <374892663@qq.com>
2019-11-07 18:18:21 +0800

    添加部署脚本文件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,53 +1,40 @@
 module.exports = {
-  title: '网站标题',
-  description: '网站描述',
+  title: '八爪鱼',
+  description: '八爪鱼平台前端文档',
   // 注入到当前页面的 HTML <head> 中的标签
   head: [
     ['link', { rel: 'icon', href: '/favicon.ico' }], // 增加一个自定义的 favicon(网页标签的图标)
   ],
-  base: '/web_accumulate/', // 这是部署到github相关的配置 下面会讲
+  base: '/docs/', // 这是部署到github相关的配置 下面会讲
   markdown: {
     lineNumbers: true // 代码块显示行号
   },
-  themeConfig: {
+  themeConfig: { //主题配置
+    // 添加导航栏
     nav: [
-      { text: '前端算法', link: '/algorithm/' }, // 内部链接 以docs为根目录
-      { text: '博客', link: 'http://obkoro1.com/' }, // 外部链接
-      // 下拉列表
+      { text: '主页', link: '/' }, // 导航条
+      { text: '组件文档', link: '/components/' },
+      { text: '知识库', link: '/knowledge/' },
       {
-        text: 'GitHub',
+        text: 'github',    // 这里是下拉列表展现形式。
         items: [
-          { text: 'GitHub地址', link: 'https://github.com/OBKoro1' },
-          {
-            text: '算法仓库',
-            link: 'https://github.com/OBKoro1/Brush_algorithm'
-          }
-        ]
-      }
-    ]
-  },
-  sidebar: {
-    // docs文件夹下面的accumulate文件夹 文档中md文件 书写的位置(命名随意)
-    '/accumulate/': [
-      '/accumulate/', // accumulate文件夹的README.md 不是下拉框形式
-      {
-        title: '侧边栏下拉框的标题1',
-        children: [
-          '/accumulate/JS/test', // 以docs为根目录来查找文件 
-          // 上面地址查找的是：docs>accumulate>JS>test.md 文件
-          // 自动加.md 每个子选项的标题 是该md文件中的第一个h1/h2/h3标题
+          { text: 'focus-outside', link: 'https://github.com/TaoXuSheng/focus-outside' },
+          { text: 'stylus-converter', link: 'https://github.com/TaoXuSheng/stylus-converter' },
         ]
       }
     ],
-    // docs文件夹下面的algorithm文件夹 这是第二组侧边栏 跟第一组侧边栏没关系
-    '/algorithm/': [
-      '/algorithm/',
-      {
-        title: '第二组侧边栏下拉框的标题1',
-        children: [
-          '/algorithm/simple/test'
-        ]
-      }
-    ]
+    // 为以下路由添加侧边栏
+    sidebar: {
+      '/baseComponents/': [
+        'MyConfirm',
+        'BatchUploading',
+        'HeadFilter',
+        'Form'
+      ],
+      '/css/': [
+        'three',
+        'four'
+      ]
+    }
   }
-};
+}
